c261e00c450f6bc0fcaa8f5a931286d965f8533b
KrisKnez <kristijankej@gmail.com>
2025-01-08 21:02:21 +0100

    temp fix

--- code/apps/backend/src/app.module.ts
+++ code/apps/backend/src/app.module.ts
@@ -6,14 +6,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     // Fix Swagger bug on Serverless (404 on assets)
-    ServeStaticModule.forRoot({
-      rootPath: require
-        .resolve('swagger-ui-dist/index.html', {
-          // paths: [require.resolve('@nestjs/swagger')],
-        })
-        .replace('index.html', ''),
-      serveRoot: '/test',
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: require
+    //     .resolve('swagger-ui-dist/index.html', {
+    //       // paths: [require.resolve('@nestjs/swagger')],
+    //     })
+    //     .replace('index.html', ''),
+    //   serveRoot: '/test',
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
