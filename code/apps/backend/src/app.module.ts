eb4b9fc1a88ea3f38b2b737f63efa294b7017146
KrisKnez <kristijankej@gmail.com>
2025-01-08 20:18:06 +0100

    fix serverless swagger

--- code/apps/backend/src/app.module.ts
+++ code/apps/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // Fix Swagger bug on Serverless (404 on assets)
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'node_modules/swagger-ui-dist'),
+      serveRoot: '/api',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
