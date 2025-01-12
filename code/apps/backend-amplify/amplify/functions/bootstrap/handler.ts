50f49c12f35a58b5d07f1ae03417c19274f22634
KrisKnez <kristijankej@gmail.com>
2025-01-12 23:58:46 +0100

    fix

--- code/apps/backend-amplify/amplify/functions/bootstrap/handler.ts
+++ code/apps/backend-amplify/amplify/functions/bootstrap/handler.ts
@@ -1,68 +1,75 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context,
-  Handler,
-} from "aws-lambda";
+// import {
+//   APIGatewayProxyEvent,
+//   APIGatewayProxyResult,
+//   Context,
+//   Handler,
+// } from "aws-lambda";
 
-import { Server } from "http";
+// import { Server } from "http";
 
-import { bootstrap } from "backend";
+// import { bootstrap } from "backend";
 
-let cachedServer: Server;
+// let cachedServer: Server;
 
-async function bootstrapServer() {
-  if (!cachedServer) {
-    const app = await bootstrap();
-    cachedServer = app.getHttpAdapter().getInstance();
-  }
-  return cachedServer;
-}
+// async function bootstrapServer() {
+//   if (!cachedServer) {
+//     const app = await bootstrap();
+//     cachedServer = app.getHttpAdapter().getInstance();
+//   }
+//   return cachedServer;
+// }
 
-export const handler: Handler = async (
-  event: APIGatewayProxyEvent,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _context: Context
-): Promise<APIGatewayProxyResult> => {
-  cachedServer = await bootstrapServer();
+// export const handler: Handler = async (
+//   event: APIGatewayProxyEvent,
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   _context: Context
+// ): Promise<APIGatewayProxyResult> => {
+//   cachedServer = await bootstrapServer();
 
-  return new Promise((resolve) => {
-    const request = {
-      method: event.httpMethod,
-      path: event.path,
-      headers: event.headers,
-      body: event.body,
-      query: event.queryStringParameters,
-      multiValueQuery: event.multiValueQueryStringParameters,
-      requestContext: event.requestContext,
-    };
+//   return new Promise((resolve) => {
+//     const request = {
+//       method: event.httpMethod,
+//       path: event.path,
+//       headers: event.headers,
+//       body: event.body,
+//       query: event.queryStringParameters,
+//       multiValueQuery: event.multiValueQueryStringParameters,
+//       requestContext: event.requestContext,
+//     };
 
-    const response: {
-      statusCode: number;
-      headers: { [key: string]: string };
-      body: string;
-      setHeader: (key: string, value: string) => void;
-      write: (chunk: any) => void;
-      end: () => void;
-    } = {
-      statusCode: 200,
-      headers: {},
-      body: "",
-      setHeader: function (key: string, value: string) {
-        this.headers[key] = value;
-      },
-      write: function (chunk: any) {
-        this.body += chunk;
-      },
-      end: function () {
-        resolve({
-          statusCode: this.statusCode,
-          headers: this.headers,
-          body: this.body,
-        });
-      },
-    };
+//     const response: {
+//       statusCode: number;
+//       headers: { [key: string]: string };
+//       body: string;
+//       setHeader: (key: string, value: string) => void;
+//       write: (chunk: any) => void;
+//       end: () => void;
+//     } = {
+//       statusCode: 200,
+//       headers: {},
+//       body: "",
+//       setHeader: function (key: string, value: string) {
+//         this.headers[key] = value;
+//       },
+//       write: function (chunk: any) {
+//         this.body += chunk;
+//       },
+//       end: function () {
+//         resolve({
+//           statusCode: this.statusCode,
+//           headers: this.headers,
+//           body: this.body,
+//         });
+//       },
+//     };
 
-    cachedServer.emit("request", request, response);
-  });
+//     cachedServer.emit("request", request, response);
+//   });
+// };
+
+import type { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event, context) => {
+  // your function code goes here
+  return 'Hello, World!';
 };
