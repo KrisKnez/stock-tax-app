585edea37fc708ac7b831228564855e85394beae
KrisKnez <kristijankej@gmail.com>
2025-01-18 02:52:02 +0100

    fix

--- code/apps/backend-amplify/amplify/functions/bootstrap/resource.ts
+++ code/apps/backend-amplify/amplify/functions/bootstrap/resource.ts
@@ -3,4 +3,16 @@ import { defineFunction } from "@aws-amplify/backend";
 export const bootstrapFunction = defineFunction({
   name: "bootstrap-function",
   entry: "./handler.ts",
+  layers: {
+    // Layers get injected into ESBuild's `external` option
+    ...Object.fromEntries(
+      [
+        "class-validator",
+        "class-transformer",
+        "@fastify/static",
+        "@nestjs/microservices",
+        "@nestjs/websockets",
+      ].map((name) => [name, "ExternalDependency:1"])
+    ),
+  },
 });
