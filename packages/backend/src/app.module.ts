b7f27d7a7f28059ffa06eddf74742170e92d1899
KrisKnez <kristijankej@gmail.com>
2025-01-22 00:19:47 +0100

    fix(backend): openapi specification

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,35 +5,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 // import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [
-    // Fix Swagger bug on Serverless (404 on assets)
-    // ServeStaticModule.forRoot({
-    //   rootPath: require
-    //     .resolve('swagger-ui-dist/package.json', {
-    //       paths: [
-    //         require
-    //           .resolve('@nestjs/swagger/package.json')
-    //           .replace('package.json', ''),
-    //       ],
-    //     })
-    //     .replace('package.json', ''),
-    //   serveRoot: '/api',
-    // }),
-    ServeStaticModule.forRoot({
-      rootPath: require(
-        require
-          .resolve('swagger-ui-dist/package.json', {
-            paths: [
-              require
-                .resolve('@nestjs/swagger/package.json')
-                .replace('package.json', ''),
-            ],
-          })
-          .replace('package.json', ''),
-      ).getAbsoluteFSPath(),
-      serveRoot: '/api',
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,15 +2,17 @@ import { bootstrap } from './bootstrap';
 
 bootstrap().then((app) => app.listen(process.env.PORT ?? 3000));
 
-// import { createRequire } from 'module';
+const swaggerUiDistPath = require
+  .resolve('swagger-ui-dist/package.json', {
+    paths: [
+      require
+        .resolve('@nestjs/swagger/package.json')
+        .replace('package.json', ''),
+    ],
+  })
+  .replace('package.json', '');
 
-// // Create a require function that is scoped to the current module URL
-// const require = createRequire(import.meta.url);
-
-// // Now you can use require.resolve()
-// const resolvedPath = require.resolve('some-package');
-
-// console.log(resolvedPath);
-
-const swaggerUiDist = require('swagger-ui-dist');
-console.log(swaggerUiDist.getAbsoluteFSPath());
+// Execute command ls on the directory of swagger-ui-dist
+console.log(
+  require('child_process').execSync(`ls ${swaggerUiDistPath}`).toString(),
+);
